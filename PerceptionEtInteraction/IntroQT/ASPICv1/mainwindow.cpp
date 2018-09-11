829c390e9082ec98b661b108dbdd90cea4d9be12
Raphael Anquetil <reallydrumz@gmail.com>
2018-09-11 16:24:07 +0200

    ajout sous menus + quit

--- PerceptionEtInteraction/IntroQT/ASPICv1/mainwindow.cpp
+++ PerceptionEtInteraction/IntroQT/ASPICv1/mainwindow.cpp
@@ -6,6 +6,31 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //Création des menus
+    QMenu *fileMenu = new QMenu("File");
+    QMenu *optionsMenu = new QMenu("Options");
+
+    ui->menuBar->addMenu(fileMenu);
+    ui->menuBar->addMenu(optionsMenu);
+
+    //Ajout des sous-menus au menu File
+    QAction *openFileAction = fileMenu->addAction("Open");
+    openFileAction->setShortcut(tr("Ctrl+O"));
+
+    QAction *saveAction = fileMenu->addAction("Save");
+    saveAction->setShortcut(tr("Ctrl+S"));
+
+    QAction *quitAction = fileMenu->addAction("Quit");
+    quitAction->setShortcut(tr("Ctrl+Q"));
+
+    //Ajout des sous-menus au menu Options
+    QAction *aboutAction = optionsMenu->addAction("About");
+    aboutAction->setShortcut(tr("Ctrl+A"));
+
+    //Ajout des événements
+    connect(quitAction, SIGNAL(triggered(bool)), qApp, SLOT(quit()));
+
 }
 
 MainWindow::~MainWindow()
